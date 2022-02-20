142513196badb49f80be4b6da816a2809b52d885
naidak <naida316@gmail.com>
2022-02-20 22:31:57 +0100

    initial import

---
+++ script.js
@@ -0,0 +1,11 @@
+function clearAll() {
+    document.getElementById("Result").innerText = '';
+}
+
+function update(value) {
+    document.getElementById("Result").innerText += value;
+}
+
+function result() {
+    document.getElementById("Result").innerText = eval(document.getElementById("Result").innerText);
+}
